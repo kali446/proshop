3b03ec420b6241ca0419422e7e1f79beb8906d35
JuniorBro <manjiljunior@gmail.com>
2021-01-04 10:34:57 +0545

    starting the frontend section completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      shopcrap
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" exact component={HomeScreen} />
+          <Route
+            path="/product/:id"
+            render={(routerProps) => <ProductScreen {...routerProps} />}
+          />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
