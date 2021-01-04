0620a701465d48b6d8d662c81026eed5260b75c2
JuniorBro <manjiljunior@gmail.com>
2021-01-04 12:18:38 +0545

    Serving and fetching data from backend section completed

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+
+  res.json(product);
+});
+
+app.listen(5000, () => console.log("server is running on port " + PORT));
